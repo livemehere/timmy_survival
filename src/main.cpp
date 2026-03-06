a8ad2193af171986650169fb11590bfc64bd1114
livemehere <rhdxoals@gmail.com>
2026-03-07 01:58:11 +0900

    feat: raylib setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() {
-  std::cout << "Hello, World" << std::endl;
+  InitWindow(1200, 800, "Timmy Survival");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello World", 600, 400, 40, BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
   return 0;
 }
