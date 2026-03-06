ba72b1f5f1f1dd95991c65234cda35ec04e0e9af
livemehere <rhdxoals@gmail.com>
2026-03-07 05:42:24 +0900

    feat:Timer.hpp

---
+++ src/Core/Timer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+class Timer {
+private:
+  float targetTime;
+  float currentTime;
+  bool isLooping;
+
+public:
+  Timer(float target, bool loop = true) {
+    targetTime = target;
+    currentTime = 0.0f;
+    isLooping = loop;
+  }
+
+  bool Update(float dt) {
+    currentTime += dt;
+
+    if (currentTime >= targetTime) {
+      if (isLooping) {
+        currentTime -= targetTime;
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  void Reset() { currentTime = 0.0f; }
+
+  void SetTargetTime(float newTarget) { targetTime = newTarget; }
+};
