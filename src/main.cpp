2db4cfb0ed630f1922ec4436410897dc24303750
livemehere <rhdxoals@gmail.com>
2026-03-15 17:11:31 +0900

    refactor: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "Managers/GameManager.hpp"
+#include "Managers/CameraManager.hpp"
+#include "Managers/ResourceManager.hpp"
 #include "raylib.h"
 
 constexpr int screenWidth = 1600;
@@ -8,14 +10,23 @@ int main() {
   SetConfigFlags(FLAG_VSYNC_HINT);
   InitWindow(screenWidth, screenHeight, "Timmy Survival");
 
-  GameManager gm;
+  GameManager &gm = GameManager::Get();
+  CameraManager &cm = CameraManager::Get();
+  ResourceManager &rm = ResourceManager::Get();
+
+  cm.Init();
   gm.Init();
+  rm.Init();
 
   while (!WindowShouldClose()) {
     gm.Update();
     gm.Draw();
   }
+
   gm.Clear();
+  cm.Clear();
+  rm.Clear();
+
   CloseWindow();
   return 0;
 }
